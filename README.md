d9d758af2ed9a3ddfb7e61d8e97d49238dcb71b4
Scott Sappenfield <scotts@Scott-Sappenfield-MBP15.local>
2019-01-27 20:43:52 -0500

    Commit required files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # AvroSpecificExample
-Simple example of creating and using a Specific Avro object
+Simple example of creating and using a Specific Avro object.
+
+Example will create a specific record as an example using a rock climbing facility as the entity.
+
+This is a Java/Maven project using JDK1.8.
+
+**Simple steps for a simple example:**
+- Write the schema in the code (not recommended, but easy to read)
+- Create an object using that schema
+- Write out a file using that schema (this will create a .avro file)
+- Read the .avro file
+
+To generate your code, use the Maven lifecycle (e.g. clean/package).
+Once done, you will be able to see your generated source (.java) and target (.class) for ClimbingGym.
---
+++ src/main/java/com/github/scotts/avro/SpecificRecordClimber.java
@@ -0,0 +1,68 @@
+package com.github.scotts.avro;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SpecificRecordClimber {
+
+    public static void main(String[] args) {
+
+        /**
+         * - Write the schema in the code (not recommended, but easy to read)
+         * - Create an object using that schema
+         * - Write out a file using that schema (this will create a .avro file)
+         * - Read the .avro file
+         */
+
+        //let's create a specific record from our generated code
+        ClimbingGym.Builder climbingGymBuilder = ClimbingGym.newBuilder();
+        climbingGymBuilder.setGymName("Inner Peaks");
+        climbingGymBuilder.setGymNickname("New Gym or South End");
+        climbingGymBuilder.setLocation("Charlotte, NC");
+        climbingGymBuilder.setHastopropeclimbing(true);
+        climbingGymBuilder.setHasleadclimbing(true);
+        climbingGymBuilder.setHasbouldering(true);
+        climbingGymBuilder.setHasspeedclimbing(true);
+        ClimbingGym climbingGym = climbingGymBuilder.build();
+
+        //let's create a file for that record
+        final DatumWriter<ClimbingGym> datumWriter = new SpecificDatumWriter<>(ClimbingGym.class);
+
+        try (DataFileWriter<ClimbingGym> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+            dataFileWriter.create(climbingGym.getSchema(), new File("climbinggym.avro"));
+            dataFileWriter.append(climbingGym);
+            System.out.println("Created file climbinggym.avro");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //let's read back that newly specific avro file, create a specific record and thumb through some fields
+        final File file = new File("climbinggym.avro");
+        final DatumReader<ClimbingGym> datumReader = new SpecificDatumReader<>(ClimbingGym.class);
+        final DataFileReader<ClimbingGym> dataFileReader;
+        try {
+            System.out.println("Reading the specific record");
+            dataFileReader = new DataFileReader<>(file, datumReader);
+            while (dataFileReader.hasNext()) {
+                ClimbingGym readClimbingGym = dataFileReader.next();
+                System.out.println(readClimbingGym.toString());
+                System.out.println("Name of Climbing Gym: " + readClimbingGym.getGymName());
+                System.out.println("Nick Name of Climbing Gym: " + readClimbingGym.getGymNickname());
+                System.out.println("Location of the Climbing Gym: " + readClimbingGym.getLocation());
+                System.out.println("Gym offers top rope climbing?: " + readClimbingGym.getHastopropeclimbing());
+                System.out.println("Gym offers lead climbing: " + readClimbingGym.getHasleadclimbing());
+                System.out.println("Gym offers bouldering: " + readClimbingGym.getHasbouldering());
+                System.out.println("Gym offers speed climbing: " + readClimbingGym.getHasspeedclimbing());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
